2d62a1348afdb7125c2f2ee80f26cad4254b59f9
Ozzie Neher <oz.neher@gmail.com>
2015-12-23 14:03:06 -0800

    Frontend setup

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Reddit Clone</title>
+    <link rel="stylesheet" href="/css/materialize.min.css" media="screen" title="no title" charset="utf-8">
+  </head>
+  <body>
+    @include('paritals.navigation')
+    @yield('content')
+    <script src="/js/jquery.min.js" charset="utf-8"></script>
+    <script src="/js/materialize.min.js" charset="utf-8"></script>
+    <script src="/js/app.js" charset="utf-8"></script>
+  </body>
+</html>
