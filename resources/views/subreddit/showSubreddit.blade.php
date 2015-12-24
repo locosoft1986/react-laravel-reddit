317ddc6438e9da2396cd3d6796596e425f3fdc7c
Ozzie Neher <oz.neher@gmail.com>
2015-12-24 13:43:31 -0800

    Started working on layout for posts

--- resources/views/subreddit/showSubreddit.blade.php
+++ resources/views/subreddit/showSubreddit.blade.php
@@ -2,4 +2,23 @@
 
 @section('content')
   <h1>{{ $subreddit->name }}</h1>
+  <div class="container">
+  @foreach ($subreddit->posts as $post)
+    <div class="row">
+    <div class="col s12 m4">
+      <div class="card">
+        <div class="card-content">
+          <span class="card-title">
+            {{$post->title}}
+          </span>
+        </div>
+        <div class="card-action">
+          <a href="#">Comments</a>
+          <a href="#">{{$post->user->username}}</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  @endforeach
+</div>
 @stop
