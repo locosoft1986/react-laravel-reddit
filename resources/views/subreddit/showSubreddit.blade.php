7066af0dabfc1ac4dd72f64ce61173ff1c52a541
Ozzie Neher <oz.neher@gmail.com>
2015-12-24 22:58:49 -0800

    Started post layout. Need to move it to React.

--- resources/views/subreddit/showSubreddit.blade.php
+++ resources/views/subreddit/showSubreddit.blade.php
@@ -4,21 +4,17 @@
   <h1>{{ $subreddit->name }}</h1>
   <div class="container">
   @foreach ($subreddit->posts as $post)
-    <div class="row">
-    <div class="col s12 m4">
-      <div class="card">
-        <div class="card-content">
-          <span class="card-title">
-            {{$post->title}}
-          </span>
-        </div>
-        <div class="card-action">
-          <a href="#">Comments</a>
-          <a href="#">{{$post->user->username}}</a>
-        </div>
+    <div class="post">
+      <div class="post-title">
+        <a href="/r/{{$subreddit->name}}/{{$post->permalink}}/{{$post->slug}}">{{$post->title}}</a>
+      </div>
+      <div class="post-meta">
+        {{\Carbon\Carbon::parse($post->created_at)->diffForHumans()}} | by {{$post->user->username}} to {{$post->subreddit->name}}
+      </div>
+      <div class="post-score">
+        {{count($post->votes)}}
       </div>
     </div>
-  </div>
   @endforeach
 </div>
 @stop
